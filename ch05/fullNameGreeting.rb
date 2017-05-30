efa6133b81b8877e72e71a12d6d6f72ee60c3106
isthisangela <angelasdong@gmail.com>
2017-05-29 20:01:11 -0700

    Create fullNameGreeting.rb

---
+++ ch05/fullNameGreeting.rb
@@ -0,0 +1,7 @@
+puts "What is your first name?"
+firstname = gets.chomp
+puts "What is your middle name?"
+midname = gets.chomp
+puts "What is your last name?"
+lastname = gets.chomp
+puts "Hello, " + firstname + " " + midname + " " + lastname + "!"
