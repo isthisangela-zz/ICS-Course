f7b1cf9a061e07168a00d044df235a11627e0900
isthisangela <angelasdong@gmail.com>
2017-05-29 21:42:09 -0700

    Update deafGrandmaExtended.rb

--- ch07/deafGrandmaExtended.rb
+++ ch07/deafGrandmaExtended.rb
@@ -1,7 +1,18 @@
-while gets.chomp != "BYE"
+byecount = 0
+while true
   n = gets.chomp
   if n != n.upcase
     puts "HUH?! SPEAK UP, SONNY!"
+    byecount = 0
   else
-    puts "NO, NOT SINCE " + (1930  + rand(21)).to_s + "!"
+    if n != "BYE"
+      puts "NO, NOT SINCE " + (1930  + rand(21)).to_s + "!"
+      byecount = 0
+    else
+      byecount = byecount + 1
+      if byecount < 3
+        puts "WHAT?!"
+      else
+        puts "SEE YOU LATER!"
+        break
 end
