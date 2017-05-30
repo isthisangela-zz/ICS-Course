bb600c1fc0b08b7402ac9e1f13375700eafcdeb5
isthisangela <angelasdong@gmail.com>
2017-05-29 20:52:43 -0700

    Create tableOfContents.rb

---
+++ ch06/tableOfContents.rb
@@ -0,0 +1,6 @@
+lwidth = 30
+rwidth = 10
+puts "Table of Contents".center(40)
+puts "Chapter 1: Getting Started".ljust(lwidth) + "page 1".rjust(rwidth)
+puts "Chapter 2: Numbers".ljust(lwidth) + "page 9".rjust(rwidth)
+puts "Chapter 3: Letters".ljust(lwidth) + "page 13".rjust(rwidth)
