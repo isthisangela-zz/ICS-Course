b7c90418b734ba1596b18be4d1c5f1d6e9626826
isthisangela <angelasdong@gmail.com>
2017-05-29 21:51:46 -0700

    Create leapYearCounter.rb

---
+++ ch07/leapYearCounter.rb
@@ -0,0 +1,19 @@
+puts "Enter start year:"
+startyr = gets.to_i
+puts "Enter end year:"
+endyr = gets.to_i
+
+year = startyr
+puts "Leap years in between:"
+while year <= endyr
+  if year % 4 == 0
+    if year % 100 == 0
+      if year % 400 == 0
+        puts year.to_s
+      end
+    else
+      puts year.to_s
+    end
+  year = year + 1
+  end
+end
