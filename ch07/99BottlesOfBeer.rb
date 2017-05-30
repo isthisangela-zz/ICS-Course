30447a57eb2abbcacbebcf8800eea492227d81f8
isthisangela <angelasdong@gmail.com>
2017-05-29 21:32:43 -0700

    Update 99BottlesOfBeer.rb

--- ch07/99BottlesOfBeer.rb
+++ ch07/99BottlesOfBeer.rb
@@ -1,9 +1,9 @@
 bottles = 99
 
-while bottles > 1
-  puts bottles + " bottles of beer on the wall, " + bottles + "bottles of beer"
+while num > 1
+  puts bottles.to_s + " bottles of beer on the wall, " + bottles.to_s + "bottles of beer"
   bottles = bottles - 1
-  puts "Take one down and pass it around, " + bottles + " bottles of beer on the wall"
+  puts "Take one down and pass it around, " + bottles.to_s + " bottles of beer on the wall"
   puts
 end
 
