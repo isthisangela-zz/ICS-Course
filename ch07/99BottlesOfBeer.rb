59ec5b66820fd9cbb83864c0cd36d1d225a87fab
isthisangela <angelasdong@gmail.com>
2017-05-29 21:24:12 -0700

    Create 99BottlesOfBeer.rb

---
+++ ch07/99BottlesOfBeer.rb
@@ -0,0 +1,14 @@
+bottles = 99
+
+while bottles > 1
+  puts bottles + " bottles of beer on the wall, " + bottles + "bottles of beer"
+  bottles = bottles - 1
+  puts "Take one down and pass it around, " + bottles + " bottles of beer on the wall"
+  puts
+end
+
+puts "1 bottle of beer on the wall, 1 bottle of beer"
+puts "Take it down and pass it around, no more bottles of beer on the wall"
+puts
+puts "No more bottles of beer on the wall, no more bottles of beer"
+puts "Go to the store and buy some more, 99 bottles of beer on the wall!"
