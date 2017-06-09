ca0b2bb20ab9520e14afe66930dbf9f6233910e6
isthisangela <angelasdong@gmail.com>
2017-06-08 23:36:12 -0700

    Create oldRoman.rb

--- ch09/oldRoman.rb
+++ ch09/oldRoman.rb
@@ -3,34 +3,13 @@ def roman n
   if (n < 1 || n > 3000)
     return 'Out of range'
   else
-    while n%1000 != n
-      r = r + 'M'
-      n = n - 1000
-    end
-    while n%500 != n
-      r = r + 'D'
-      n = n - 500
-    end
-    while n%100 != n
-      r = r + 'C'
-      n = n - 100
-    end
-    while n%50 != n
-      r = r + 'L'
-      n = n - 50
-    end
-    while n%10 != n
-      r = r + 'X'
-      n = n - 10
-    end
-    while n%5 != n
-      r = r + 'V'
-      n = n - 5
-    end
-    while n > 0
-      r = r + 'I'
-      n = n - 1
-    end
+    r = r + 'M'*(n/1000)
+    r = r + 'D'*(n%1000/500)
+    r = r + 'C'*(n%500/100)
+    r = r + 'L'*(n%100/50)
+    r = r + 'X'*(n%50/10)
+    r = r + 'V'*(n%10/5)
+    r = r + 'I'*(n%5)
   end
   r
 end
