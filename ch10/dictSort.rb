ad291f74112107dbd8391dd2acb095825d86cf26
Angela Dong <angelasdong@gmail.com>
2017-06-30 18:07:17 +0800

    Update dictSort.rb

--- ch10/dictSort.rb
+++ ch10/dictSort.rb
@@ -4,13 +4,13 @@ end
 
 def recursive_sort unsorted_array, sorted_array
   if unsorted_array.length == 0
-    sorted_array
+    return sorted_array
   else
     lowercase_array = []
     unsorted_array.each{|n| lowercase_array.push(n.downcase)}
     small = lowercase_array.min
     unsorted_array.delete_at(lowercase_array.index(small))
     sorted_array.push(small)
-    recursive_sort unsorted_array, sorted_array
+    return recursive_sort unsorted_array, sorted_array
   end
 end
