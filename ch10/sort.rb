f608b4050c0d9bbda36a9d083e44e1bf99802b62
Angela Dong <angelasdong@gmail.com>
2017-06-30 17:33:33 +0800

    Update sort.rb

--- ch10/sort.rb
+++ ch10/sort.rb
@@ -1,1 +1,14 @@
+def sort some_array
+  recursive_sort some_array, [] 
+end 
 
+def recursive_sort unsorted_array, sorted_array
+  if unsorted_array.length == 0
+    sorted_array
+  else
+    small = unsorted_array.min
+    unsorted_array.delete_at(unsorted_array.index(small))
+    sorted_array.push(small)
+    recursive_sort unsorted_array, sorted_array
+  end
+end
