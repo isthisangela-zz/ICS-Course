24ecdea5731b8bb82ee72695e32335fdbb3d99af
Angela Dong <angelasdong@gmail.com>
2017-06-30 18:06:43 +0800

    Update shuffle.rb

--- ch10/shuffle.rb
+++ ch10/shuffle.rb
@@ -4,12 +4,12 @@ end
 
 def recursive_shuffle unshuffled_array, shuffled_array
   if unshuffled_array.length == 0
-    shuffled_array
+    return shuffled_array
   else
     ind = rand(unshuffled_array.length)
     next = unshuffled_array[ind]
     unshuffled_array.delete_at(ind)
     shuffled_array.push(next)
-    recursive_shuffle unshuffled_array, shuffled_array
+    return recursive_shuffle unshuffled_array, shuffled_array
   end
 end
