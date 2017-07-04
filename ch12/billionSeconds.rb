c545b619a0b0f124ca8b50d176fbd4a3cc723159
Angela Dong <angelasdong@gmail.com>
2017-07-04 21:13:56 +0800

    Update billionSeconds.rb

--- ch12/billionSeconds.rb
+++ ch12/billionSeconds.rb
@@ -1,3 +1,18 @@
-birth = Time.local(2000, 25, 10, 07, 30)
+puts "Enter birth year:"
+year = gets.chomp.to_i
+
+puts "Enter birthdate (mm/dd):"
+birthdate = gets.chomp
+month = birthdate[0..1].to_i
+day = birthdate[3..4].to_i
+
+puts "Enter time of birth in 24h format (xx:xx):"
+time = gets.chomp
+hour = time[0..1].to_i
+minute = time[3..4].to_i
+
+birth = Time.local(year, month, day, hour, minute)
 billion = birth + 1000000000
-puts billion
+
+puts "You will be a billion seconds old at the following time:"
+puts billion.to_s[0..-7] # remove time zone
