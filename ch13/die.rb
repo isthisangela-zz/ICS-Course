d3840dccae3e6f47d17d3498ff668bbbd7499179
Angela Dong <angelasdong@gmail.com>
2017-07-04 23:08:16 +0800

    Update die.rb

--- ch13/die.rb
+++ ch13/die.rb
@@ -1,1 +1,25 @@
-
+class Die
+  
+  def initialize
+    roll
+  end
+  
+  def roll
+    @number_showing = 1 + rand(6)
+  end
+  
+  def showing
+    @number_showing
+  end
+  
+  def cheat x
+    while x < 1
+      x = x + 6
+    end
+    while x > 6
+      x = x - 6
+    end
+    @number_showing = x
+  end
+  
+end
