b102ceff9a3b1668e31d53f6dd0ac644c95c4a6a
Angela Dong <angelasdong@gmail.com>
2017-07-04 18:12:06 +0800

    Update englishNumber.rb

--- ch10/englishNumber.rb
+++ ch10/englishNumber.rb
@@ -12,7 +12,47 @@ def english_number number
   tens_place = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'] 
   teenagers = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'] 
   
-  left = number 
+  left = number
+  
+  write = left/1000000000000
+  left = left - write*1000000000000
+  if write > 0
+    trillions = english_number write
+    num_string = num_string + trillions + ' trillion'
+    if left > 0
+      num_string = num_string + ' '
+    end
+  end
+  
+  write = left/1000000000
+  left = left - write*1000000000
+  if write > 0
+    billions = english_number write
+    num_string = num_string + billions + ' billion'
+    if left > 0
+      num_string = num_string + ' '
+    end
+  end
+  
+  write = left/1000000
+  left = left - write*1000000
+  if write > 0
+    millions = english_number write
+    num_string = num_string + millions + ' million'
+    if left > 0
+      num_string = num_string + ' '
+    end
+  end
+  
+  write = left/1000
+  left = left - write*1000
+  if write > 0
+    thousands = english_number write
+    num_string = num_string + thousands + ' thousand'
+    if left > 0
+      num_string = num_string + ' '
+    end
+  end
   
   write = left/100
   left = left - write*100
