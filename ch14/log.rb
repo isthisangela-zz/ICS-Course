6e6bce29f6c6ea97062f516a113d1a36d6b745f9
Angela Dong <angelasdong@gmail.com>
2017-07-27 17:39:54 -0700

    Update log.rb

--- ch14/log.rb
+++ ch14/log.rb
@@ -1,1 +1,16 @@
+def log &block, name
+  puts "Beginning '" + name + "'..."
+  val = block.call
+  puts "'" + name + "' finished, returning:"
+  puts val
+end
 
+log "outer_block" do
+  log "some_little_block" do
+    5
+  end
+  log "yet_another_block" do
+    "I like Thai food!"
+  end
+  false
+end
