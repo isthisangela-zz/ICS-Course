094efe85cf0b5bdc69fe7cc4c11777f638d8a763
Angela Dong <angelasdong@gmail.com>
2017-07-27 17:34:43 -0700

    Update profiling.rb

--- ch14/profiling.rb
+++ ch14/profiling.rb
@@ -1,1 +1,14 @@
-
+def profile block_description, &block
+  
+  #change to turn profiling on
+  profiling = false
+  
+  if profiling
+    start_time = Time.new
+    block.call
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block.call
+  end
+end
